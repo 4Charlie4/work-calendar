bd331b078d3a7f4b055e5e0fd8597d38ca9d999c
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-07-13 11:07:52 -0400

    working on bg color

--- Develop/script.js
+++ Develop/script.js
@@ -2,3 +2,27 @@
 const timeNow = moment().format("dddd, MMMM Do, h:mm");
 $("#currentDay").text(timeNow);
 
+//code that will cause past timeblocks to gray out
+var timePass = function () {
+    var time = $(".hour")
+    .text()
+    .trim()
+    console.log(time);
+    
+    if (moment().isAfter(time)) {
+        
+        $(".hour").addClass("past");
+    } 
+    
+
+}
+timePass();
+
+//present timeblock is red
+
+//future timeblocks are green
+
+// save input information into localstorage upon button click
+
+
+
